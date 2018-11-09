80b303788f0463fcbbe058de2ded993d435161b8
alan-fang <fang.alan.7@gmail.com>
2018-11-09 17:26:52 -0600

    remove unused require

--- server/src/game_api.js
+++ server/src/game_api.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const multer = require('multer')
-const https = require('https')
 const axios = require('axios')
 const fs = require('fs')
 const keys = require('./creds.json')
