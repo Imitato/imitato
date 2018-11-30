c7bf71c9223976bb57ebb6933fa15a5b3951f7aa
Angad Garg <angad.garg3@gmail.com>
2018-11-30 03:24:05 -0600

    more gamemaster

--- src/views/game.jsx
+++ src/views/game.jsx
@@ -5,24 +5,32 @@ class Game extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      game_id: ''
+      game_id: '',
+      rounds: []
     }
   }
 
-  getGame = () => {
+  createRound = () => {
     var gameId = this.state.game_id
     var data = new FormData();
 
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
 
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-        console.log(this.responseText);
+    xhr.addEventListener("readystatechange", () => {
+        if (xhr.readyState === 4) {
+          this.setState({rounds: JSON.parse(xhr.response).rounds})
+          let xhr2 = new XMLHttpRequest()
+          xhr2.withCredentials = true;
+
+          xhr2.open("GET", "/imitato/game/start_round?gameId=" + gameId);
+          xhr2.setRequestHeader("cache-control", "no-cache");
+
+          xhr2.send(data);
         }
     });
 
-    xhr.open("GET", "/imitato/game?id=" + gameId);
+    xhr.open("GET", "/imitato/game/create_round?gameId=" + gameId);
     xhr.setRequestHeader("cache-control", "no-cache");
 
     xhr.send(data);
@@ -56,11 +64,30 @@ class Game extends Component {
           <p>Set up a game of Imitato, a fun game where you imitate ideas with your friends and have the computer guess.</p>
 
           <div>
-              <button id="createGameButton" onClick={this.createGame}>Create game</button>
-              gameId:
-              <input id="gameIdBox" value={this.state.game_id} onChange={(e) => {this.setState({game_id: e.target.value})}} />
-              <button id="getGameButton" onClick={this.getGame}>Get game</button>
+              <button id="createGameButton" onClick={this.createGame}>Create game</button><br />
+              <h4 id="gameIdBox">GAME ID: {this.state.game_id}</h4>
+              <button id="getGameButton" onClick={this.createRound}>Start Round</button>
           </div>
+          {this.state.rounds && this.state.rounds.length ? (
+            <>
+              <div>Round {this.state.rounds.length}</div>
+              <ul>
+                {Object.keys(this.state.rounds[this.state.rounds.length - 1].emotions_map).map(emotion => (
+                  this.state.rounds[this.state.rounds.length - 1].emotions_map[emotion] > 0 ? (
+                    <li>{emotion}</li>
+                  ) : (
+                    <></>
+                  )
+                ))}
+              </ul>
+              {    Object.keys(this.state.rounds[this.state.rounds.length - 1].emotions_map).map(emotion => {
+            console.log(emotion)
+            console.log(this.state.rounds[this.state.rounds.length - 1].emotions_map[emotion] > 0)
+          })}
+            </>
+          ) : (
+            <></>
+          )}
       </>
     )
   }
