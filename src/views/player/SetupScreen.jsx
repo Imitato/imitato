69d19160799e82e8884c61d1885d80d06e22477a
Steve Zhu <st.zhu1@gmail.com>
2018-11-30 19:45:27 -0600

    Create setup screen

---
+++ src/views/player/SetupScreen.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default class SetupScreen extends React.Component {
+  render() {
+    return (
+      <Styles>
+        <img src="images/imitato.png"/>
+        <div className="container">
+          <label htmlFor="game-id">Game Code</label>
+          <input
+            name="gameId"
+            id="game-id"
+            type="text"
+            placeholder="Enter 6 digit code"
+          />
+          <label htmlFor="user-id">Name</label>
+          <input
+            name="userId"
+            id="user-id"
+            type="text"
+            placeholder="Enter your name"
+          />
+          <button>Join</button>
+        </div>
+      </Styles>
+    )
+  }
+}
+
+const Styles = styled.div`
+  padding: 2em;
+
+  img {
+    width: 100%;
+    margin-bottom: 20px;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    margin-bottom: 0.5em;
+  }
+
+  input {
+    margin-bottom: 1em;
+    padding: 0.5em;
+    box-shadow: 0 0px 4px rgba(0, 0, 0, 0.3);
+    border: 1px solid gray;
+    border-radius: 0.2em;
+
+    text-transform: uppercase;
+  }
+
+  button {
+    border-radius: 0.4em;
+    padding: 0.5em;
+    background-color: #3f51b5;
+    color: white;
+    text-transform: uppercase;
+    font-size: 20px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+`
