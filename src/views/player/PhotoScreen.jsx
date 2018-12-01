d45af228d5e9d83af6880d5572371604b5a0a965
Steve Zhu <st.zhu1@gmail.com>
2018-12-01 12:43:28 -0600

    Add button for taking and retaking photos

--- src/views/player/PhotoScreen.jsx
+++ src/views/player/PhotoScreen.jsx
@@ -9,13 +9,19 @@ const USER_MEDIA_CONSTRAINTS = {
 }
 
 export default class PhotoScreen extends React.Component {
-  state = { streaming: false }
+  state = { streaming: false, photoTaken: false }
+
+  constructor(props) {
+    super(props)
+    this.video = React.createRef()
+    this.canvas = React.createRef()
+  }
 
   componentDidMount() {
     navigator.mediaDevices
       .getUserMedia(USER_MEDIA_CONSTRAINTS)
       .then(stream => {
-        video.srcObject = stream
+        this.video.current.srcObject = stream
         this.setState({ streaming: true })
       })
       .catch(console.error)
@@ -25,12 +31,38 @@ export default class PhotoScreen extends React.Component {
     return (
       <Styles>
         {!this.state.streaming && <LoadingView />}
-        <video ref={this.video} id="video" autoPlay playsInline>
-          Video stream not available.
-        </video>
-        <button className="photo-button" />
+        <div style={{ display: this.state.photoTaken ? 'none' : 'block' }}>
+          <video ref={this.video} className="photo" autoPlay playsInline>
+            Video stream not available.
+          </video>
+          <button id="photo-button" onClick={this.takePhoto} />
+        </div>
+        <div style={{ display: this.state.photoTaken ? 'block' : 'none' }}>
+          <canvas ref={this.canvas} className="photo" />
+          <button
+            id="retake-button"
+            onClick={() => this.setState({ photoTaken: false })}
+          >
+            ×
+          </button>
+        </div>
       </Styles>
     )
+  }
+
+  takePhoto = () => {
+    const video = this.video.current
+    const canvas = this.canvas.current
+
+    const width = video.videoWidth
+    const height = video.videoHeight
+
+    const context = canvas.getContext('2d')
+    canvas.width = width
+    canvas.height = height
+    context.drawImage(video, 0, 0, width, height)
+
+    this.setState({ photoTaken: true })
   }
 }
 
@@ -41,15 +73,15 @@ const Styles = styled.div`
   overflow: hidden;
   border: none;
 
-  video {
+  .photo {
     object-fit: cover;
-    width: calc(100% + 1px);
-    height: 100%;
+    width: calc(100vw + 1px);
+    height: 100vh;
     border: none;
     transform: scale(-1, 1);
   }
 
-  .photo-button {
+  #photo-button {
     position: absolute;
     left: 0;
     right: 0;
@@ -64,5 +96,22 @@ const Styles = styled.div`
     background: url('https://upload.wikimedia.org/wikipedia/commons/5/5b/Rotten_Tomatoes.svg')
       no-repeat center;
     background-size: contain;
+    cursor: pointer;
+  }
+
+  #retake-button {
+    position: absolute;
+    left: 0.2em;
+    top: 0.2em;
+    margin: 0;
+    padding: 0;
+    border: none;
+    background: none;
+
+    color: white;
+    font-size: 50px;
+    text-shadow: 0px 0px 1px black;
+    line-height: 0.6;
+    cursor: pointer;
   }
 `
